e61cad9edb42488aee853b58b3a38d744447212c
Slava Melanko <slava.melanko@gmail.com>
2026-03-31 21:49:24 +0300

    Remove redundant fallbackLng from web i18n init

--- apps/web/src/i18n.js
+++ apps/web/src/i18n.js
@@ -6,11 +6,10 @@ import { createI18n } from '@smela/i18n'
 import HttpBackend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
-import { DEFAULT_LOCALE, loadLocale } from '@/lib/userPreferences'
+import { loadLocale } from '@/lib/userPreferences'
 
 const i18n = await createI18n({
   lng: loadLocale(),
-  fallbackLng: DEFAULT_LOCALE,
   backend: {
     loadPath: '/locales/{{lng}}.json'
   },
