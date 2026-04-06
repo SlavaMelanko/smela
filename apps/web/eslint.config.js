af61d50f542b8cfd2ab6ba4eeea922f81af9bafe
Slava Melanko <slava.melanko@gmail.com>
2026-04-06 22:15:02 +0300

    Remove redundant node globals override for config files

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,18 +1,8 @@
 import { playwrightConfig, reactConfig, shadcnConfig } from '@smela/eslint'
-import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
   ...reactConfig,
   ...shadcnConfig,
-  ...playwrightConfig,
-  {
-    // Build and config files
-    files: ['*.config.js'],
-    languageOptions: {
-      globals: {
-        ...globals.node
-      }
-    }
-  }
+  ...playwrightConfig
 ]
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,20 +1,10 @@
 import { reactConfig, shadcnConfig, vitestConfig } from '@smela/eslint'
 import storybook from 'eslint-plugin-storybook'
-import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
   ...reactConfig,
   ...shadcnConfig,
   ...vitestConfig,
-  ...storybook.configs['flat/recommended'],
-  {
-    // Build and config files
-    files: ['*.config.js'],
-    languageOptions: {
-      globals: {
-        ...globals.node
-      }
-    }
-  }
+  ...storybook.configs['flat/recommended']
 ]
